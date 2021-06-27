4ba6671b9596f1beec3041927e3c143980e800cf
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-06-27 18:39:04 -0500

    Update file

--- 4.1 Funciones2.sql
+++ 4.1 Funciones2.sql
@@ -25,10 +25,14 @@ BEGIN
 	RETURN @paginas;
 END//
 
+CREATE FUNCTION obtener_ventas()
+RETURNS INT
+BEGIN
+  SET @paginas = (SELECT (ROUND( RAND() * 100 ) * 6 ));
+  RETURN @paginas;
+END//
+
 DELIMITER ;
 
-SELECT obtener_paginas();
-
-UPDATE  libros  SET paginas=obtener_paginas();
-
-SELECT * FROM libros;
+UPDATE libros SET paginas = obtener_paginas();
+UPDATE libros SET ventas = obtener_ventas();
